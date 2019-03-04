ccf68e5f2adc97c0e008e29fe23e1519588dd2cf
soonlung pong <soon_lung_pong@manulife.com>
2019-03-04 17:38:23 +0800

    convict

--- src/index.js
+++ src/index.js
@@ -7,9 +7,14 @@ let app = express();
 let bodyParser = require('body-parser');
 let mongoose = require('mongoose');
 
+const config = require('./config.js');
+//console.log(config.get('db.connectionString'));
+
 app.use(bodyParser.urlencoded({extended:true}));
 
-mongoose.connect('mongodb://localhost/carts');
+//mongoose.connect('mongodb://localhost/carts');
+mongoose.connect(config.get('db.connectionString'));
+
 var db = mongoose.connection;
 
 //import routes
@@ -17,7 +22,9 @@ let apiRoutes = require('./routes')
 app.use('/api', apiRoutes);
 
 // Setup server port
-var port = process.env.PORT || 9191;
+//var port = process.env.PORT || 9191;
+var port = config.get('server.port')
+
 // Send message for default URL
 app.get('/', (req, res) => res.send('Carts API'));
 
